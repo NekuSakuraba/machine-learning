9331df58bcc587d525bb501d56b5345f2816513e
Rodrigo Miike <drimiksi@gmail.com>
2017-09-27 15:35:44 -0300

    New implementation of the multivariate t

--- projects/capstone/project/multivariate_t_mixture.py
+++ projects/capstone/project/multivariate_t_mixture.py
@@ -1,57 +1,55 @@
 import numpy as np
+from numpy import log as ln
+
 from sklearn.cluster import KMeans
-from scipy.linalg import cholesky, solve_triangular, LinAlgError, inv
+
+from scipy.linalg import cholesky, solve_triangular, LinAlgError
 from scipy.special import gammaln, digamma, logsumexp
 
+def _compute_precision_cholesky(covariances):
+    n_components, n_features, _ = covariances.shape
+
+    precisions_chol = np.empty((n_components, n_features, n_features))
+    for k, covariance in enumerate(covariances):
+        try:
+            cov_chol = cholesky(covariance, lower=True)
+        except LinAlgError:
+            raise ValueError('covariance: {0}'.format(covariance))
+        precisions_chol[k] = solve_triangular(cov_chol, np.eye(n_features), lower=True).T
+    return precisions_chol
+
 class MultivariateTMixture:
-    def __init__(self, n_components=1, max_iter=16, random_state=None, reg_covar=1e-6, tol=1e-3, init_params='kmeans'):
+    def __init__(self, n_components=1, max_iter=100, random_state=None, reg_covar=1e-6, tol=1e-3):
         self.n_components = n_components
         self.random_state = random_state
-        self.max_iter = max_iter
+
+        self.max_iter  = max_iter
         self.reg_covar = reg_covar
+
         self.tol = tol
-        self.init_params = init_params
 
     def _initialize_parameters(self, X):
         n_components = self.n_components
-        n_samples, n_features = X.shape
-
-        # initializing tau
-        if self.init_params == 'kmeans':
-            tau = np.zeros((n_samples, n_components))
-            label = KMeans(n_clusters=n_components, random_state=self.random_state).fit(X).labels_
-            tau[np.arange(n_samples), label] = 1
-        else:
-            np.random.seed(self.random_state)
-            tau = np.random.rand(n_samples, n_components)
-            tau = tau / tau.sum(axis=1)[:, np.newaxis]
 
         # initializng u
-        u = np.ones((n_samples, self.n_components))
+        u = np.ones((self.n_samples, self.n_components))
+
+        # initializing tau
+        tau = np.zeros((self.n_samples, n_components))
+        label = KMeans(n_clusters=n_components, random_state=self.random_state).fit(X).labels_
+        tau[np.arange(self.n_samples), label] = 1
 
         return tau, u
 
-    def _compute_precision_cholesky(self, sigma):
-        n_components, n_features, _ = sigma.shape
-
-        precisions_chol = np.empty((n_components, n_features, n_features))
-        for k, covariance in enumerate(sigma):
-            try:
-                cov_chol = cholesky(covariance, lower=True)
-            except LinAlgError:
-                raise ValueError('covariance: {0}'.format(covariance))
-            precisions_chol[k] = solve_triangular(cov_chol, np.eye(n_features), lower=True).T
-        return precisions_chol
-
-    def _estimate_covariances(self, X, mu, pi, tau, u):
-        n_components, n_features = mu.shape
-        sigma = np.empty((n_components, n_features, n_features))
+    def _estimate_covariances(self, X, means, n, tau, u):
+        n_components, n_features = means.shape
+        covariances = np.empty((n_components, n_features, n_features))
         for k in range(n_components):
-            diff = X - mu[k]
-            sigma[k] = np.dot(tau[:, k] * u[:, k] * diff.T, diff) / pi[k]
-            sigma[:: n_features + 1] += self.reg_covar
+            diff = X - means[k]
+            covariances[k] = np.dot(tau[:, k] * u[:, k] * diff.T, diff) / n[k]
+            covariances[:: n_features + 1] += self.reg_covar
 
-        return sigma
+        return covariances
 
     def _estimate_parameters(self, X, tau, u):
         """
@@ -64,37 +62,49 @@ class MultivariateTMixture:
 
         covariances :
         """
-        pi = tau.sum(0)  # pi without normalizing
+        n = tau.sum(0)  # pi without normalizing
         mu = np.dot(tau.T * u.T, X) / np.sum(tau * u, 0)[:, np.newaxis]
-        sigma = self._estimate_covariances(X, mu, pi, tau, u)
+        covariances = self._estimate_covariances(X, mu, n, tau, u)
 
-        return pi, mu, sigma
+        return n, mu, covariances
 
     def _compute_q1(self, tau):
-        return tau * np.log(self.pi)
+        return tau * ln(self.pi) # normalized pi
 
-    def _compute_q2(self, df, tau, u):
+    def _compute_q2(self, tau, u):
         q2 = np.empty(tau.shape)
         for k in range(self.n_components):
-            q2[:, k] = tau[:, k] * self._compute_q2_helper(df[k], u[:, k])
+            q2[:, k] = tau[:, k] * self._compute_q2_helper(u[:, k], self.df[k])
         return q2
 
-    def _compute_q2_helper(self, df, u):
-        half_df = df * .5
+    def _compute_q2_helper(self, u, df):
+        half_df   = df * .5
         half_feat = self.n_features * .5
 
-        return -gammaln(half_df) + half_df * np.log(half_df) \
-               + half_df * (digamma(half_df + half_feat) - np.log(half_df + half_feat) \
-                            + np.sum(np.log(u) - u)/len(u))
+        return -gammaln(half_df) + half_df*ln(half_df) \
+               +half_df*(digamma(half_df+half_feat) - ln(half_df+half_feat) \
+                            + np.sum(ln(u) - u))
 
     def _compute_q3(self, tau, u, mahalanobis):
-        log_det_chol = (np.sum(np.log(
-            self.precisions_chol.reshape(
-                self.n_components, -1)[:, ::self.n_features + 1]), 1))
+        # log_det_chol = np.sum(ln(self.precisions_chol.reshape(self.n_components, -1)[:, ::self.n_features + 1]), axis=1)
+        log_det = self._compute_log_det()
 
         log_prob = mahalanobis * u
-        q3 = .5 * (self.n_features * (np.log(u) - np.log(2 * np.pi)) - log_prob) + log_det_chol
+        q3 = .5 * (self.n_features*(ln(u) - ln(2 * np.pi)) - log_prob) + log_det
         return q3 * tau
+
+    def _compute_log_likelihood(self, X, tau, u):
+        q1 = self._compute_q1(tau)
+        q2 = self._compute_q2(tau, u)
+
+        self.precisions_chol = _compute_precision_cholesky(self.covariances)
+
+        mahalanobis = self._compute_mahalanobis(X)
+        q3 = self._compute_q3(tau, u, mahalanobis)
+
+        weighted_log_prob = q1 + q2 + q3
+        log_norm_prob = logsumexp(weighted_log_prob, axis=1)
+        return np.mean(log_norm_prob)
 
     def _compute_mahalanobis(self, X):
         n_samples, _ = X.shape
@@ -109,51 +119,44 @@ class MultivariateTMixture:
         return (self.df + self.n_features) / (mahalanobis + self.df)
 
     def fit(self, X):
-        n_samples, n_features = X.shape
-        self.n_features = n_features
+        self.n_samples, self.n_features = X.shape
 
         tau, u = self._initialize_parameters(X)
 
-        pi, means, sigmas = self._estimate_parameters(X, tau, u)
-        pi /= n_samples  # normalizing
-        self.pi = pi
+        n, means, covariances = self._estimate_parameters(X, tau, u)
+        self.pi = n/self.n_samples # normalizing
+
+        # precision cholesky
+        self.precisions_chol = _compute_precision_cholesky(covariances)
 
         df = np.ones(self.n_components) * 4
 
         self.means = means
-        self.sigmas = sigmas
+        self.covariances = covariances
         self.df = df
-
-        # precision cholesky
-        self.precisions_chol = self._compute_precision_cholesky(sigmas)
-
-        # mahalanobis = self._compute_mahalanobis(X)
-        # u = self._compute_u(mahalanobis)
 
         self.lower_bound = -np.inf
 
-        likelihood = []
         for _ in range(self.max_iter):
             prev_lower_bound = self.lower_bound
 
-            log_prob_norm, tau, u = self._e_step_1(X, tau, u)
+            log_resp, u = self._e_step_1(X, tau)
+            tau = np.exp(log_resp)
+
             self._cm_step_1(X, tau, u)
 
             u = self._e_step_2(X)
-            self._cm_step_2(np.exp(tau), u)
+            self._cm_step_2(tau, u)
 
-            self.lower_bound = np.mean(log_prob_norm)
-            likelihood.append(self.lower_bound)
+            self.lower_bound = self._compute_log_likelihood(X, tau, u)
+
             change = self.lower_bound - prev_lower_bound
-
-            tau = np.exp(tau)
 
             if abs(change) < self.tol:
                 print 'done with #{} iterations'.format(_)
                 break
-        self.likelihood = likelihood
 
-    def _e_step_1(self, X, tau, u):
+    def _e_step_1(self, X, tau):
         """ E-step 01 - It calculates the loglikelihood under the current parameters
             and estimates the parameters tau and u.
 
@@ -166,38 +169,36 @@ class MultivariateTMixture:
         tau : array-like, shape(n_samples, n_components)
 
         u   : array-like, shape(n_samples, n_components)
-
         """
-        n_samples, n_features = X.shape
+        # E-step 01
+        n_components, n_features = self.n_components, self.n_features
 
-        q1 = self._compute_q1(tau)  # precisa do pi aqui
-        q2 = self._compute_q2(self.df, tau, u)  # precisa das features e do DF
+        log_det = self._compute_log_det()
 
-        mahalanobis = self._compute_mahalanobis(X)
-        q3 = self._compute_q3(tau, u, mahalanobis)
+        log_prob = self._compute_mahalanobis(X)
 
-        weighted_log_prob = q1 + q3 + q2
+        half_df = self.df * .5
+        half_p  = n_features * .5
+        weighted_log = gammaln(half_df+half_p) - gammaln(half_df) \
+                       -(half_df + half_p)*(ln(self.df + log_prob) - ln(self.df)) \
+                       -half_p*ln(np.pi * self.df) + log_det + ln(tau.sum(axis=0))
+        # TODO Verificar se o tau precisa ser normalizado
 
-        # logexpsum trick?
-        xmin = np.array([min(x) for x in weighted_log_prob ]).reshape(-1, 1)
+        log_prob_norm = logsumexp(weighted_log, axis=1)[:, np.newaxis]
 
-        log_prob_norm = logsumexp(weighted_log_prob-xmin, axis=1)[:, np.newaxis] + xmin
-        log_resp = weighted_log_prob - log_prob_norm
+        log_resp = weighted_log - log_prob_norm
 
-        # estimating 'u'
-        new_u = self._compute_u(mahalanobis)
+        u = (self.df + n_features) / (self.df + log_prob)
 
-        # returning tau, u
-        return log_prob_norm, log_resp, new_u
+        return log_resp, u
 
     def _cm_step_1(self, X, tau, u):
         n_samples, _ = X.shape
 
-        pi, self.means, self.sigmas = self._estimate_parameters(X, np.exp(tau), u)
-        pi /= n_samples  # normalizing
-        self.pi = pi
+        n, self.means, self.covariances = self._estimate_parameters(X, tau, u)
+        self.pi = n/n_samples  # normalizing
 
-        self.precisions_chol = self._compute_precision_cholesky(self.sigmas)
+        self.precisions_chol = _compute_precision_cholesky(self.covariances)
 
     def _e_step_2(self, X):
         # computing the mahalanobis distance
@@ -209,17 +210,26 @@ class MultivariateTMixture:
         n = tau.sum(axis=0)
         tau_u = np.sum(tau * (np.log(u) - u), axis=0)
 
-        new_df = np.empty(self.df.shape)
+        df = np.empty(self.df.shape)
         for k in range(self.n_components):
-            best = (self.df[k], 0)
+            best = self.df[k]
             step = 10.
             for __ in range(5):
-                for _ in np.arange(best[0], best[0] + step, step / 10):
-                    result = -digamma(_ * .5) + np.log(_ * .5) + 1 + digamma(((_ + 2) * .5)) - np.log(((_ + 2) * .5)) + tau_u[k] / n[k]
+                for v in np.arange(best, best + step, step / 10.):
+                    half_v = v * .5
+                    half_p = self.n_features * .5
+
+                    result = -digamma(half_v) + np.log(half_v) + 1 \
+                             +digamma(half_v+half_p) - np.log(half_v+half_p) \
+                             +tau_u[k]/n[k]
+
                     if result > 0:
-                        best = (_, result)
-                    # else:
-                    #     break
+                        best = v
+                    else:
+                        break
                 step /= 10.
-            new_df[k] = best[0]
-        self.df = new_df
+            df[k] = best
+        self.df = df
+
+    def _compute_log_det(self):
+        return np.sum(ln(self.precisions_chol.reshape(self.n_components, -1)[:, ::self.n_features + 1]), axis=1)
--- projects/capstone/project/multivariate_test.py
+++ projects/capstone/project/multivariate_test.py
@@ -2,31 +2,69 @@ import numpy as np
 from multivariate_util import multivariate_t_rvs
 from multivariate_t_mixture import MultivariateTMixture
 
-mu  = [0, 0]
-cov = np.eye(2)
-X1 = multivariate_t_rvs(mu, cov, 4, 350)
+def two_components():
+    np.random.seed(0)
+    X1 = np.random.randn(350, 2) * .5 + 7
+    X2 = np.random.randn(350, 2) * 1.1
 
-mu  = [5, 5]
-cov = np.eye(2) * .5
-X2 = multivariate_t_rvs(mu, cov, 19, 350)
+    X = np.r_[X1, X2]
 
-mu  = [-5, -5]
-cov = np.eye(2) * 2
-X3 = multivariate_t_rvs(mu, cov, 7.5, 350)
+    t = MultivariateTMixture(n_components=2, random_state=0)
+    t.fit(X)
 
-# X = np.concatenate([X1, X2, X3])
-# t = MultivariateTMixture(3, max_iter=100)
+    print 'means'
+    print t.means
 
-X = np.concatenate([X1, X2])
-t = MultivariateTMixture(2, max_iter=100)
+    print 'covariances'
+    print t.covariances
 
-t.fit(X)
+    print 'df'
+    print t.df
 
-print t.means
-print t.sigmas
-print t.df
+def two_components_2():
+    X1 = multivariate_t_rvs([7, 7], [[.5,0], [0,.5]], 7.5, 350)
+    X2 = multivariate_t_rvs([0, 0], [[1.1,0], [0,1.1]], 18.33, 350)
+    X = np.r_[X1, X2]
 
-import matplotlib.pyplot as plt
-arr = range(len(t.likelihood))
-plt.scatter(arr, t.likelihood)
-plt.show()
+    t = MultivariateTMixture(n_components=2, random_state=0)
+    t.fit(X)
+
+    print 'means'
+    print t.means
+
+    print 'covariances'
+    print t.covariances
+
+    print 'df'
+    print t.df
+
+def three_components():
+    np.random.seed(0)
+    X1 = np.random.randn(350, 2) * .5 + 7
+    X2 = np.random.randn(350, 2) * 1.1
+    X3 = np.random.randn(350, 2) * .333 -2
+
+    X = np.r_[X1, X2, X3]
+
+    t = MultivariateTMixture(n_components=3, random_state=0)
+    t.fit(X)
+
+    # import matplotlib.pyplot as plt
+    # plt.scatter(X1[:,0], X1[:,1])
+    # plt.scatter(X2[:,0], X2[:,1])
+    # plt.scatter(X3[:,0], X3[:,1])
+    # plt.scatter(t.means[:, 0], t.means[:, 1], c='r', marker='x')
+    #
+    # plt.show()
+
+    print 'means'
+    print t.means
+
+    print 'covariances'
+    print t.covariances
+
+    print 'df'
+    print t.df
+
+if __name__ == '__main__':
+    two_components_2()
